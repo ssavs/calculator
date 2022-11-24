3c2c9a68381a61bf2399794549b8d16973f47aee
Savannah Samuels <savannahvermeulen10@gmail.com>
2022-11-24 15:58:13 +0200

    first commit

--- index.js
+++ index.js
@@ -1,11 +1,11 @@
 
-let outputScreen = document.getElementById("output-screen");
+let outputScreen = document.getElementById("outputScreen");
+// let num = "";
+
 function display (num){
     outputScreen.value += num;
 }
-function Calculate (){
-        outputScreen.value = eval (outputScreen.value);
-     }
+
     function Clear(){
         outputScreen.value = "";
     }
@@ -14,14 +14,14 @@ function Calculate (){
         outputScreen.value = outputScreen.value.slice(0,-1);
     }
 
-function addToSum(operator){
-
-}
+// function addToSum(operator){
+// num = num + operator;
+// document.getElementById('outputScreen').value = num;
+// }
 
 function evaluateSum() {
-    outputScreen.value = eval (outputScreen.value);
+    // outdocument.getElementById('outputScreen').innerText
+    outputScreen.value = eval(outputScreen.value);
 }
 
-function clearSum(){
-    outputScreen.value="";
-}
+
