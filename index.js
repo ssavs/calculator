5f2ff5e755ac738243e589810f263e19674926dc
Savannah Samuels <savannahvermeulen10@gmail.com>
2022-11-26 10:24:30 +0200

    first commit

--- index.js
+++ index.js
@@ -11,21 +11,23 @@ function displayOpe (num){
     outputScreen.value = outputScreen.value + num;
     allowComa = true;
 }
+
 function displaydot (num){
     if (allowComa === true) {
         outputScreen.value = outputScreen.value + num;
         allowComa = false;
     }
-
+    
 }
-    function Clear(){
+
+function Clear(){
         outputScreen.value = "";
         allowComa = true
     }
 
-    function Del (){
+function Del (){
         outputScreen.value = outputScreen.value.slice(0,-1);
-    }
+}
 
 // function addToSum(operator){
 // num = num + operator;
